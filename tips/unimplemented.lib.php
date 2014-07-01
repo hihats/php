1e78a35b4229224abd2c33b7c5c19e6d3fd507a0
Hisahiro Tsukamoto <hishats@gmail.com>
2014-07-01 12:27:54 +0900

    bugfix

--- tips/unimplemented.lib.php
+++ tips/unimplemented.lib.php
@@ -10,7 +10,7 @@
 if(version_compare(phpversion(), "5.5.0", '<')){
 	function array_column($array, $column){
 		$ret = array();
-		foreach($array as $row) $ret = $row[$column];
+		foreach($array as $row) $ret[] = $row[$column];
 		return $ret;
 	}
 }
