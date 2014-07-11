c976a1704ce58b1a8e1af0209a37bd67b8a8a7d8
Hisahiro Tsukamoto <hishats@gmail.com>
2014-07-11 20:54:15 +0900

    add function json_encode() for old version php environment

--- tips/unimplemented.lib.php
+++ tips/unimplemented.lib.php
@@ -14,3 +14,39 @@ if(version_compare(phpversion(), "5.5.0", '<')){
 		return $ret;
 	}
 }
+
+/**
+ * http://jp2.php.net/manual/en/function.json-encode.php
+ */
+if(version_compare(phpversion(), "5.2.0", '<')){
+	function json_encode($val){
+		if (is_int($val)||is_float($val)) return $val;
+		if (is_bool($val)) return $val?'true':'false';
+		if (is_array($val)) {
+			$len = count($val);
+			for($ii=0; $ii<$len && isset($val[$ii]); ++$ii) {}
+			$temp = array();
+			if($len==$ii) {
+			for($i=0;$i<$len;$i++) {
+			$temp[] = sprintf("%s", json_encode($val[$i]));
+			}
+			return '['. implode(",",$temp) .']';
+			} else {
+			foreach($val as $key => $item) {
+			$temp[] = sprintf("%s:%s", json_encode($key), json_encode($item));
+			}
+			return '{'. implode(",", $temp) .'}';
+			}
+ 		}
+		// else string
+		static $from = array('\\',  "\n", "\r", '"');
+		static $to   = array('\\\\','\\n','\\r','\\"');
+		static $cmap = array(0x80, 0xFFFF, 0, 0xFFFF);
+		return '"'. preg_replace_callback(
+		'/&#([0-9]+);/',
+		create_function('$match','return sprintf("\\u%04x", $match[1]);'),
+		mb_encode_numericentity(str_replace($from, $to, $val), $cmap, 'UTF-8')
+		) . '"';
+ 	}
+}
+
